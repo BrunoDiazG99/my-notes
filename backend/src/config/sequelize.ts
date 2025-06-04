c7b78b871f56e563864460ddaf98e17bd4ffc510
Haruchon <brunodiazg99@gmail.com>
2025-06-04 16:37:41 -0500

    tested deployed db

--- backend/src/config/sequelize.ts
+++ backend/src/config/sequelize.ts
@@ -1,13 +1,18 @@
 import { Sequelize } from "sequelize";
-
-const orm = new Sequelize("my-notes", "root", "paltita99sql", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 1,
-    idle: 10000,
-    acquire: 60000,
-  },
-});
+const orm = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: 5432,
+    dialect: "postgres",
+    pool: {
+      max: 1,
+      idle: 10000,
+      acquire: 60000,
+    },
+  }
+);
 
 export default orm;
